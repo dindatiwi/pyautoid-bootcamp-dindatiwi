2878a07f8d10680f9894dba10fddac028113e68e
Adinda Pratiwi Prameswari <44132935+dindatiwi@users.noreply.github.com>
2021-09-04 20:32:26 +0700

    fix to add the website name

--- tugas01.py
+++ tugas01.py
@@ -1,9 +1,10 @@
 from selenium import webdriver
 
 driver = webdriver.Chrome(executable_path='/Users/dindatiwi/Downloads/chromedriver')
-websites = ["https://noobtest.id/","https://erzaf.com/","https://www.orangsiber.com/","https://demoqa.com/","https://automatetheboringstuff.com/"]
+websites = ["https://noobtest.id","https://erzaf.com","https://orangsiber.com","https://demoqa.com","https://automatetheboringstuff.com"]
 for i in websites:
     driver.minimize_window()
     driver.get(i)
-    print(driver.title)
+    title = i.split("/",2)
+    print(title[2]," - ",driver.title)
     driver.close
