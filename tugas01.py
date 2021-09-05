87c4e2e5453bcf70db80d4dfb6f5fb07e28e8feb
Adinda Pratiwi Prameswari <44132935+dindatiwi@users.noreply.github.com>
2021-09-05 10:48:46 +0700

    kesekian kali nya perbaiki posisi, this one must be work !

--- tugas01.py
+++ tugas01.py
@@ -2,8 +2,8 @@ from selenium import webdriver
 
 driver = webdriver.Chrome(executable_path='/Users/dindatiwi/Downloads/chromedriver')
 websites = ["https://noobtest.id","https://erzaf.com","https://orangsiber.com","https://demoqa.com","https://automatetheboringstuff.com"]
+driver.minimize_window()
 for i in websites:
-    driver.minimize_window()
     driver.get(i)
     title = i.split("/")
     print(title[2], " - ", driver.title)
