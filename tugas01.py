0bfb4963aa14087e809982b3e465f4c7138db624
Adinda Pratiwi Prameswari <44132935+dindatiwi@users.noreply.github.com>
2021-09-05 10:41:37 +0700

    fix typo

--- tugas01.py
+++ tugas01.py
@@ -3,8 +3,8 @@ from selenium import webdriver
 driver = webdriver.Chrome(executable_path='/Users/dindatiwi/Downloads/chromedriver')
 websites = ["https://noobtest.id","https://erzaf.com","https://orangsiber.com","https://demoqa.com","https://automatetheboringstuff.com"]
 for i in websites:
-    driver.get(i)
     driver.minimize_window()
+    driver.get(i)
     title = i.split("/")
     print(title[2], " - ", driver.title)
-    driver.close
+driver.close()
